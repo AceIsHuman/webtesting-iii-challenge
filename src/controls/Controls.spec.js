1b8cbb153c2340eaf5877dad9737a1c7787155c3
AceIsHuman <menoman1995@yahoo.com>
2019-10-02 18:39:16 -0500

    test if gate can be opened while locked

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,11 @@
-// Test away!
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+
+import Controls from './Controls';
+
+test("cannot be opened if locked", () => {
+  const toggleClosed = jest.fn();
+  const { getByText } = render(<Controls toggleClosed={toggleClosed} locked={true} closed={true}/>);
+  fireEvent.click(getByText(/open gate/i));
+  expect(toggleClosed).not.toBeCalled();
+});
