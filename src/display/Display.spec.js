532d5ea0e92b9128e2ba4df88ed77da0bc118f91
AceIsHuman <menoman1995@yahoo.com>
2019-10-02 18:58:28 -0500

    test if state of gate is rendered

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,10 @@
-// Test away!
+import React from 'react'
+import { render } from '@testing-library/react';
+
+import Display from './Display';
+
+test("displays state of gate", () => {
+  const { getByText } = render(<Display />);
+    getByText(/open/i);
+    getByText(/unlocked/i);
+})
