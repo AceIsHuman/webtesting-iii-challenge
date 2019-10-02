fcf8f3ab725ced9e4046cf2e3dd9ed0fc295190b
AceIsHuman <menoman1995@yahoo.com>
2019-10-02 18:16:45 -0500

    test component renders unlocked and open

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,10 @@
-// Test away
+import React from 'react'
+import { render } from '@testing-library/react';
+
+import Dashboard from './Dashboard';
+
+test("gate default to unlocked and open", () => {
+  const { getByText } = render(<Dashboard />);
+  expect(getByText(/unlocked/i)).toBeTruthy();
+  expect(getByText(/open/i)).toBeTruthy();
+});
